832464c8904a47f9adfb1bb24e2ddcb9218b5051
REVSIX <69701767+REVSIX@users.noreply.github.com>
2023-08-27 02:10:57 -0400

    Commented Code

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -1,15 +1,19 @@
+#Import Libraries
 from bs4 import BeautifulSoup
 
+#Open Local HTML File
 with open('home.html', 'r') as html_file:
     content = html_file.read()
 
+    #New Instance of BeatifulSoup
     soup = BeautifulSoup(content, 'lxml')
+    
+    #Search Request 
     course_cards  = soup.find_all('div', class_ = "card")
+    
+    #Format Scraped Data
     for course in course_cards:
         course_name = course.h5.text
         course_price = course.a.text.split()[-1]
 
         print(f"{course_name} costs {course_price}.")
-
-
-
